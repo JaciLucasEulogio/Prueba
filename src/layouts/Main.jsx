9043583fd74227c7dbf66b7a6622afcf04e10f2f
jaci annet <72617519@continental.edu.pe>
2024-05-08 01:49:21 -0500

    Resolved conflicts in Main.jsx

--- src/layouts/Main.jsx
+++ src/layouts/Main.jsx
@@ -2,10 +2,7 @@ import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Navbar from '../components/Navbar';
 import '../App.css';
-<<<<<<< HEAD
-=======
 import Footer from '../components/Footer';
->>>>>>> 58fca7f9576d41c54bf50c36e887b6ef9eed94b3
 
 const Main = () => {
     return (
